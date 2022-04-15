4b9ffee0aff31a0c09ba9100db6201972fdfe9f8
Glitch (glitch-blank-website) <none>
2022-04-15 22:44:35 +0000

    🍉🏢 Checkpoint ./script.js:43915665/1008 ./index.html:43915665/87 ./style.css:43915665/145

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 /* If you're feeling fancy you can add interactivity 
     to your site with Javascript */
 
+// global constants
+const clueHoldTime = 1000; //how long to hold each clue's light/sound
+const cluePauseTime = 333; //how long to pause in between clues
+const nextClueWaitTime = 1000; //how long to wait before starting playback of the clue sequence
+
+
 //Global Variables
 var pattern = [2, 2, 4, 3, 2, 1, 2, 4];
 var progress = 0; 
@@ -28,7 +34,7 @@ const freqMap = {
   2: 329.6,
   3: 392,
   4: 466.2
-  5: 232
+
 }
 function playTone(btn,len){ 
   o.frequency.value = freqMap[btn]
@@ -63,3 +69,29 @@ g.connect(context.destination)
 g.gain.setValueAtTime(0,context.currentTime)
 o.connect(g)
 o.start(0)
+
+function lightButton(btn){
+  document.getElementById("button"+btn).classList.add("lit")
+}
+function clearButton(btn){
+  document.getElementById("button"+btn).classList.remove("lit")
+}
+
+function playSingleClue(btn){
+  if(gamePlaying){
+    lightButton(btn);
+    playTone(btn,clueHoldTime);
+    setTimeout(clearButton,clueHoldTime,btn);
+  }
+}
+
+function playClueSequence(){
+  context.resume()
+  let delay = nextClueWaitTime; //set delay to initial wait time
+  for(let i=0;i<=progress;i++){ // for each clue that is revealed so far
+    console.log("play single clue: " + pattern[i] + " in " + delay + "ms")
+    setTimeout(playSingleClue,delay,pattern[i]) // set a timeout to play that clue
+    delay += clueHoldTime 
+    delay += cluePauseTime;
+  }
+}
